d1587691a366bccb76e6956d8f0e0028339935f1
david <a22300906@ceti.mx>
2026-05-31 22:25:35 -0600

    Correccion de firebase

--- backend/config/firebase.config.js
+++ backend/config/firebase.config.js
@@ -1,13 +1,12 @@
 const admin = require('firebase-admin');
-const path = require('path');
 require('dotenv').config();
 
-const serviceAccount = require(
-  path.resolve(process.env.FIREBASE_CREDENTIALS_PATH)
-);
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  })
 });
 
 console.log('✅ Firebase Admin inicializado');
